39e9c72791682a1f5b4630c5b6fda38e3cd67fce
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-02-14 12:14:47 -0300

    create list

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,11 @@
 <h3>Principal(View)</h3>
+
+<li>
+    <a href="/">Principal</a>
+</li>
+<li>
+    <a href="/sobre-nos">Sobre Nós</a>
+</li>
+<li>
+    <a href="/contato">Contato</a>
+</li>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,11 @@
 <h3>Sobre nós(View)</h3>
+
+<li>
+    <a href="/">Principal</a>
+</li>
+<li>
+    <a href="/sobre-nos">Sobre Nós</a>
+</li>
+<li>
+    <a href="/contato">Contato</a>
+</li>
