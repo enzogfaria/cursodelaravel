92e0c04e671b829bae25594adb3ea4260cd5210b
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-02-22 16:50:40 -0300

    att

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        print_r($request->all());
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+
         return view('site.contato', ['titulo'=> ' - Contato']);
     }
 
